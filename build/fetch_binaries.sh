8722a798558a3661d0b1d183dc6b1f31fd77f760
Andrea Cervesato <andrea.cervesato@gmail.com>
2024-01-07 09:57:05 +0100

    Update fetch_binaries.sh

--- build/fetch_binaries.sh
+++ build/fetch_binaries.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -exuo pipefail
 
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
