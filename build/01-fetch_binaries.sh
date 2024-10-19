418ea864b3c1a0312d71769b233d598632b0928a
Andrea Cervesato <andrea.cervesato@gmail.com>
2024-10-19 20:51:00 +0200

    Manage multi arch

--- build/01-fetch_binaries.sh
+++ build/01-fetch_binaries.sh
@@ -7,6 +7,9 @@ get_latest_release() {
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
 
+function validate_url(){
+  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then return true;else return false; fi
+}
 
 ARCH=$(uname -m)
 case $ARCH in
@@ -24,13 +27,21 @@ mkdir $BINDIR
 get_ctop() {
   VERSION=$(get_latest_release bcicen/ctop | sed -e 's/^v//')
   LINK="https://github.com/bcicen/ctop/releases/download/v${VERSION}/ctop-${VERSION}-linux-${ARCH}"
-  wget "$LINK" -O $BINDIR/ctop && chmod +x $BINDIR/ctop
+  if [ $(validate_url $LINK) ]; then
+    wget "$LINK" -O $BINDIR/ctop && chmod +x $BINDIR/ctop
+  else
+    echo no CTOP for ${ARCH}
+  fi
 }
 
 get_calicoctl() {
   VERSION=$(get_latest_release projectcalico/calicoctl)
   LINK="https://github.com/projectcalico/calicoctl/releases/download/${VERSION}/calicoctl-linux-${ARCH}"
-  wget "$LINK" -O $BINDIR/calicoctl && chmod +x $BINDIR/calicoctl
+  if [ $(validate_url $LINK) ]; then
+    wget "$LINK" -O $BINDIR/calicoctl && chmod +x $BINDIR/calicoctl
+  else
+    echo no calicoctl for ${ARCH}
+  fi
 }
 
 get_termshark() {
@@ -46,21 +57,32 @@ get_termshark() {
         TERM_ARCH="$ARCH"
       fi
       LINK="https://github.com/gcla/termshark/releases/download/v${VERSION}/termshark_${VERSION}_linux_${TERM_ARCH}.tar.gz"
-      wget "$LINK" -O $BINDIR/termshark.tar.gz && \
-      tar -zxvf $BINDIR/termshark.tar.gz && \
-      mv "termshark_${VERSION}_linux_${TERM_ARCH}/termshark" $BINDIR/termshark && \
-      chmod +x $BINDIR/termshark
+      if [ $(validate_url $LINK) ]; then
+        wget "$LINK" -O $BINDIR/termshark.tar.gz && \
+        tar -zxvf $BINDIR/termshark.tar.gz && \
+        mv "termshark_${VERSION}_linux_${TERM_ARCH}/termshark" $BINDIR/termshark && \
+        chmod +x $BINDIR/termshark
+      else
+        echo no termshark for ${ARCH}
+      fi
+
       ;;
   esac
 }
 
 
 get_oc() {
-  LINK="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz"
-  wget "$LINK" -O $BINDIR/oc.tar.gz && \
-  cd $BINDIR && \
-  tar -zxvvf $BINDIR/oc.tar.gz && \
-  chmod +x $BINDIR/oc $BINDIR/kubectl
+  LINK="https://mirror.openshift.com/pub/openshift-v4/${ARCH}/clients/ocp/stable/openshift-client-linux.tar.gz"
+  if [ $(validate_url $LINK) ]; then
+    wget "$LINK" -O $BINDIR/oc.tar.gz && \
+    cd $BINDIR && \
+    tar -zxvvf $BINDIR/oc.tar.gz && \
+    chmod +x $BINDIR/oc
+    unlink $BINDIR/kubectl
+  else
+    echo no oc for ${ARCH}
+  fi
+
 }
 
 get_ctop
