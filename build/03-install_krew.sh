0b56606e1661d310d026105b28be68e44f7758bb
Andrea Cervesato <andrea.cervesato@gmail.com>
2024-10-19 21:21:50 +0200

    Debug

--- build/03-install_krew.sh
+++ build/03-install_krew.sh
@@ -22,6 +22,7 @@ OS="$(uname | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
 KREW="krew-${OS}_${ARCH}"
 LINK="https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz"
+echo LINK=$LINK
 if [ $(validate_url $LINK) ]; then
   curl -fsSLO $LINK
   tar zxvf "${KREW}.tar.gz"
@@ -32,7 +33,7 @@ if [ $(validate_url $LINK) ]; then
   if [ $(command -v kubectl) ]; then
     kubectl krew install $KREWPLUGINS
   else
-    echo Unable to find krew plugins
+    echo Unable to find kubectl executable. Krew is anyway hot and ready to use.
   fi
 else
   color_echo 31 "no krew for ${ARCH}"
--- build/99-install_shell_utils.sh
+++ build/99-install_shell_utils.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+if [ ! $DISABLESHELL_UTILS ]; then
+  # Install oh-my-zsh
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  # Install spaceship-prompt
+  git clone "https://github.com/spaceship-prompt/spaceship-prompt.git" "/.oh-my-zsh/custom/themes/spaceship-prompt" --depth=1
+  ln -s "/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
 
-# Install spaceship-prompt
-git clone "https://github.com/spaceship-prompt/spaceship-prompt.git" "/.oh-my-zsh/custom/themes/spaceship-prompt" --depth=1
-ln -s "/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
+  # Install zsh-autosuggestions
+  git clone "https://github.com/zsh-users/zsh-autosuggestions.git" "/.oh-my-zsh/custom//plugins/zsh-autosuggestions" --depth=1
 
-# Install zsh-autosuggestions
-git clone "https://github.com/zsh-users/zsh-autosuggestions.git" "/.oh-my-zsh/custom//plugins/zsh-autosuggestions" --depth=1
+  # Install oh-my-tmux
+  git clone https://github.com/gpakosz/.tmux.git /.tmux --depth=1
+  ln -s -f .tmux/.tmux.conf /.tmux.conf
+  cp .tmux/.tmux.conf.local /.tmux.conf.local
+  echo "set-option -g default-shell /bin/zsh" >> /.tmux.conf.local
 
-# Install oh-my-tmux
-git clone https://github.com/gpakosz/.tmux.git /.tmux --depth=1
-ln -s -f .tmux/.tmux.conf /.tmux.conf
-cp .tmux/.tmux.conf.local /.tmux.conf.local
-echo "set-option -g default-shell /bin/zsh" >> /.tmux.conf.local
+  # Install vim plugins
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  vim +PlugInstall +qall
 
-# Install vim plugins
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
-
-# Correct permissions
-chgrp -R 0  /.zshrc /.oh-my-zsh && chmod -R g=u /.zshrc /.oh-my-zsh  /.vim
-chmod +x /entrypoint.sh
+  # Correct permissions
+  chgrp -R 0  /.zshrc /.oh-my-zsh && chmod -R g=u /.zshrc /.oh-my-zsh  /.vim
+  chmod +x /entrypoint.sh
+fi
